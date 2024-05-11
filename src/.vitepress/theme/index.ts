6248d42a29a261544cc294993fab31bde9e14108
Cloudea <2025140062@qq.com>
2024-05-11 13:57:50 +0800

    style: Add custom css

--- src/.vitepress/theme/index.ts
+++ src/.vitepress/theme/index.ts
@@ -3,6 +3,7 @@ import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import Layout from './Layout.vue'
 import "./style.css";
+import './custom.css'
 
 export default {
   extends: DefaultTheme,
